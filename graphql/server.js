7b31f9e6e425f806e80e5303ec8c1be17fe9bacf
MayyarAA <mayyar1235@gmail.com>
2022-05-15 21:17:36 -0400

    more complex books obj

--- graphql/server.js
+++ graphql/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const { authors, books } = require('./fakeData.js');
+
 const { graphqlHTTP } = require('express-graphql');
 const {
 	GraphQLSchema,
@@ -8,28 +10,52 @@ const {
 	GraphQLList,
 	GraphQLInt,
 	GraphQLNonNull,
+	GraphQLID,
 } = require('graphql');
 
-//declaring new graphQLschema
-const schema = new GraphQLSchema({
+const BookTypeQL = new GraphQLObjectType({
+	name: 'Book',
+	description: 'Book object',
+	fields: () => ({
+		id: { type: GraphQLNonNull(GraphQLID) },
+		name: { type: GraphQLNonNull(GraphQLString) },
+		authorId: { type: GraphQLNonNull(GraphQLInt) },
+	}),
+});
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQuery',
+	description: 'Root Query',
+	fields: () => ({
+		books: {
+			type: new GraphQLList(BookTypeQL),
+			description: 'List of all books',
+			resolve: () => books,
+		},
+	}),
+});
+// declaring new graphQLschema
+const schemaSimple = new GraphQLSchema({
 	//declaring new graphQLobj
 	query: new GraphQLObjectType({
 		name: 'FirstGraphQLObjs',
 		fields: () => ({
 			message: {
 				type: GraphQLString,
-				resolve: () => {
-					'First GraphQLSchemaObj';
-				},
+				resolve: () => 'First GraphQLSchemaObj',
 			},
 		}),
 	}),
 });
 
+const schemaComplex = new GraphQLSchema({
+	query: RootQuery,
+});
+
 app.use(
 	'/graphql',
 	graphqlHTTP({
-		schema: schema,
+		schema: schemaComplex,
 		graphiql: true,
 	})
 );
