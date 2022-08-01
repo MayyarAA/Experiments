f52deaac595faa0b1629e610c52d4e59466787c2
MayyarAA <mayyar1235@gmail.com>
2022-08-01 12:26:20 -0400

    springboot p1

---
+++ java/springtraining1/SpringOne/src/main/java/com/mayyar/springone/SpringOne/SpringOneApplication.java
@@ -0,0 +1,13 @@
+package com.mayyar.springone.SpringOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOneApplication.class, args);
+	}
+
+}
