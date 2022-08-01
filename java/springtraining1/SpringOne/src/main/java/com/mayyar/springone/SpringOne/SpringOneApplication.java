3c68c41528ee35b96fc2e4ce0fd7e6579cd10252
MayyarAA <mayyar1235@gmail.com>
2022-08-01 19:18:31 -0400

    working clean build of api

--- java/springtraining1/SpringOne/src/main/java/com/mayyar/springone/SpringOne/SpringOneApplication.java
+++ java/springtraining1/SpringOne/src/main/java/com/mayyar/springone/SpringOne/SpringOneApplication.java
@@ -2,6 +2,8 @@ package com.mayyar.springone.SpringOne;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
 
 @SpringBootApplication
 public class SpringOneApplication {
@@ -9,5 +11,12 @@ public class SpringOneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringOneApplication.class, args);
 	}
-
+	@RestController
+	@RequestMapping("/api")
+	public class ApiController{
+		@GetMapping("/greeting")
+		public String getGreeting(){
+			return "Hello World from api";
+		}
+	}
 }
