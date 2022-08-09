2b5858fc6edf7deb1bb2966910beb4fff8eb67bf
MayyarAA <mayyar1235@gmail.com>
2022-08-09 00:19:05 -0400

    include syntax

--- courses/cplusplus/test.cpp
+++ courses/cplusplus/test.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
     string name = "Tom";
-    cout << "hello world2 " << name;
+    cout << "hello world2\n"
+         << name << "\n";
+    cout << pow(10.0, 2.0);
     // cout << "hello world";
     return 0;
 }
