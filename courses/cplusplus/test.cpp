dec36cdd39350773629ea2d3bb9afeaeed609bf3
MayyarAA <mayyar1235@gmail.com>
2022-08-09 01:47:42 -0400

    pointers & classes

--- courses/cplusplus/test.cpp
+++ courses/cplusplus/test.cpp
@@ -2,12 +2,33 @@
 #include <cmath>
 using namespace std;
 
+class Book
+{
+public: //attributes of class
+    string title;
+    string author;
+    int pages;
+    Book(string aTitle, string aAuthor, int aPages) //constructor
+    {
+        cout << "Creating Book object";
+        title = aTitle;
+        author = aAuthor;
+        pages = aPages;
+    }
+};
+
 int main()
 {
-    string name = "Tom";
-    cout << "hello world2\n"
-         << name << "\n";
-    cout << pow(10.0, 2.0);
-    // cout << "hello world";
+    // string fullName = "Tom";
+    // string *pFullName = &fullName; //* is the actual variable's pointer/memory address
+    // cout << "hello world2, waht is your name\n";
+    // getline(cin, fullName);
+    // cout << "hello " << fullName << " this is the memory "
+    //      << "address of the variable fullName => " << pFullName;
+    // int value1 = 10;
+    // int *pValue1 = &value1;
+    // cout << "\n address of value1 " << *pValue1;
+    Book book1("Harry Botter", "JK Rol", 254);
+    cout << book1.author;
     return 0;
 }
