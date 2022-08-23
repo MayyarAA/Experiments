be43c83b43f3559b008b6f8271463d9dc79dccb5
MayyarAA <mayyar1235@gmail.com>
2022-08-23 13:45:33 -0400

    complex fnc calls

--- typescript/demo/dist/test.js
+++ typescript/demo/dist/test.js
@@ -1,25 +1,75 @@
 // const a = 'hello';
 // console.log('this is the console ' + a);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var tes = 20;
 var helperFcn1 = function (data1, data2) {
     return data1 + ' ' + data2;
 };
-var user1 = {
-    name: 'Tom',
-    id: 10,
-    getMessage: function () {
-        return 'hello from ' + this.name;
-    }
-};
-var pageNumber = 10;
-pageNumber = '15';
+// interface IUser {
+// 	name: string;
+// 	id: number;
+// 	getMessage(): string;
+// }
+// const user1: IUser = {
+// 	name: 'Tom',
+// 	id: 10,
+// 	getMessage() {
+// 		return 'hello from ' + this.name;
+// 	},
+// };
+// let pageNumber: string | number = 10;
+// pageNumber = '15';
 // let value1: number;
 // console.log(value1, value1);
 var customLog = function (nameOfValue, value) {
-    console.log("".concat(nameOfValue, " ").concat(typeof value, " w/ value ").concat(value));
+    console.log("".concat(JSON.stringify(nameOfValue), " typeof ").concat(typeof value, " w/ value ").concat(JSON.stringify(value)));
     console.log('---------------------------------------------------');
 };
 var value2 = 10;
-customLog(Object.keys({ value2: value2 })[0], value2);
+// customLog(Object.keys({ value2 })[0], value2);
 value2 = null;
-customLog(Object.keys({ value2: value2 })[0], value2);
+var popularTags = ['good', 'happy'];
+var helperFcn2 = function (data) {
+    //do some work
+    console.log('here in void fcn');
+};
+var page = '10';
+var pageNumber = page;
+var UserClass = /** @class */ (function () {
+    function UserClass(firstName, id) {
+        this.firstName = firstName;
+        this.id = id;
+    }
+    UserClass.prototype.getFirstName = function () {
+        return this.firstName;
+    };
+    UserClass.prototype.getMessage = function () {
+        return 'hello message';
+    };
+    return UserClass;
+}());
+var AdminUserClass = /** @class */ (function (_super) {
+    __extends(AdminUserClass, _super);
+    function AdminUserClass(firstName, id, adminSecretKey) {
+        var _this = _super.call(this, firstName, id) || this;
+        _this.adminSecretKey = adminSecretKey;
+        return _this;
+    }
+    return AdminUserClass;
+}(UserClass));
+var adminUser1 = new AdminUserClass('TomeUser1', 10, 'secretKEy10202102010');
+customLog(Object.keys({ user2: adminUser1 })[0], adminUser1);
--- typescript/demo/src/test.ts
+++ typescript/demo/src/test.ts
@@ -1,10 +1,61 @@
 // const a = 'hello';
 // console.log('this is the console ' + a);
-
+import * as R from 'ramda';
 let tes: number = 20;
 const helperFcn1 = (data1: string, data2: number): string => {
 	return data1 + ' ' + data2;
 };
+
+// interface IUser {
+// 	name: string;
+// 	id: number;
+// 	getMessage(): string;
+// }
+
+// const user1: IUser = {
+// 	name: 'Tom',
+// 	id: 10,
+// 	getMessage() {
+// 		return 'hello from ' + this.name;
+// 	},
+// };
+
+// let pageNumber: string | number = 10;
+
+// pageNumber = '15';
+
+// let value1: number;
+
+// console.log(value1, value1);
+const customLog = (nameOfValue, value) => {
+	console.log(
+		`${JSON.stringify(nameOfValue)} typeof ${typeof value} w/ value ${JSON.stringify(value)}`
+	);
+	console.log('---------------------------------------------------');
+};
+
+type CustomNumberAlias = number | null;
+let value2: CustomNumberAlias = 10;
+// customLog(Object.keys({ value2 })[0], value2);
+
+value2 = null;
+// customLog(Object.keys({ value2 })[0], value2);
+
+type PopularTag = string;
+
+const popularTags: PopularTag[] = ['good', 'happy'];
+
+const helperFcn2 = (data): void => {
+	//do some work
+	console.log('here in void fcn');
+};
+
+let page: string = '10';
+let pageNumber: number = (page as unknown) as number;
+// customLog(pageNumber, pageNumber);
+// console.log('pageNumber', pageNumber);
+
+//classes
 
 interface IUser {
 	name: string;
@@ -12,29 +63,35 @@ interface IUser {
 	getMessage(): string;
 }
 
-const user1: IUser = {
-	name: 'Tom',
-	id: 10,
+class UserClass implements IUser {
+	private firstName: string;
+	name: string;
+	id: number;
+	constructor(firstName: string, id: number) {
+		this.firstName = firstName;
+		this.id = id;
+	}
+	getFirstName(): string {
+		return this.firstName;
+	}
 	getMessage() {
-		return 'hello from ' + this.name;
-	},
-};
+		return 'hello message';
+	}
+}
 
-let pageNumber: string | number = 10;
+class AdminUserClass extends UserClass {
+	private adminSecretKey: string;
+	constructor(firstName: string, id: number, adminSecretKey: string) {
+		super(firstName, id);
+		this.adminSecretKey = adminSecretKey;
+	}
+}
 
-pageNumber = '15';
+const adminUser1 = new AdminUserClass('TomeUser1', 10, 'secretKEy10202102010');
+customLog(Object.keys({ user2: adminUser1 })[0], adminUser1);
 
-// let value1: number;
+///reading custom code
 
-// console.log(value1, value1);
-const customLog = (nameOfValue, value) => {
-	console.log(`${nameOfValue} ${typeof value} w/ value ${value}`);
-	console.log('---------------------------------------------------');
-};
-
-type CustomNumberAlias = number | null;
-let value2: CustomNumberAlias = 10;
-customLog(Object.keys({ value2 })[0], value2);
-
-value2 = null;
-customLog(Object.keys({ value2 })[0], value2);
+const searchStr = 'hello';
+const arrTest = ['hello', 'type', 'test'];
+// const updatedNumArray = R.any<string>((el: string) => el.contains(searchStr), arrTest);
