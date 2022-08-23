2c18c9320877b94c26a878bd9fe3423a9dadab99
MayyarAA <mayyar1235@gmail.com>
2022-08-23 15:48:52 -0400

    ssss:wq!

---
+++ java/springtraining1/SpringOne/src/main/java/com/mayyar/springone/SpringOne/DockerTest/HelloController.java
@@ -0,0 +1,7 @@
+package com.mayyar.springone.SpringOne.DockerTest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+}
--- java/springtraining1/SpringOne/src/main/java/com/mayyar/springone/SpringOne/SpringOneApplication.java
+++ java/springtraining1/SpringOne/src/main/java/com/mayyar/springone/SpringOne/SpringOneApplication.java
@@ -6,17 +6,22 @@ import org.springframework.web.bind.annotation.*;
 
 
 @SpringBootApplication
+@RestController
 public class SpringOneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringOneApplication.class, args);
 	}
-	@RestController
-	@RequestMapping("/api")
-	public class ApiController{
-		@GetMapping("/greeting")
-		public String getGreeting(){
-			return "Hello World from api";
-		}
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
 	}
+////	@RestController
+//	@RequestMapping("/api")
+//	public class ApiController{
+//		@GetMapping("/greeting")
+//		public String getGreeting(){
+//			return "Hello World from api";
+//		}
+//	}
 }
