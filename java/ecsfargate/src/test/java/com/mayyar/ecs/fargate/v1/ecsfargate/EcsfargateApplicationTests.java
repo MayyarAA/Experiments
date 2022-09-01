311f1d169365d0593d734be625a41209ea14843b
MayyarAA <mayyar1235@gmail.com>
2022-09-01 01:08:19 -0400

    able to link notes to page

---
+++ java/ecsfargate/src/test/java/com/mayyar/ecs/fargate/v1/ecsfargate/EcsfargateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayyar.ecs.fargate.v1.ecsfargate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsfargateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
