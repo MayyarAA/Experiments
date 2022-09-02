884a19d4d0c9de276fc721e9545963c8e10d6d42
MayyarAA <mayyar1235@gmail.com>
2022-09-01 21:17:57 -0400

    gitignro files

--- java/ecsfargate/src/main/java/com/mayyar/ecs/fargate/v1/ecsfargate/Controller/FirstController.java
+++ java/ecsfargate/src/main/java/com/mayyar/ecs/fargate/v1/ecsfargate/Controller/FirstController.java
@@ -1,2 +1,19 @@
-package com.mayyar.ecs.fargate.v1.ecsfargate.Controller;public class FirstController {
+package com.mayyar.ecs.fargate.v1.ecsfargate.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class FirstController {
+    @GetMapping
+    public String applicationStatus(){
+        return "App is running fully healthy ";
+    }
+    @GetMapping("/{name}")
+    public String welcome(@PathVariable String name){
+        return "Hello " + name + " this is the ECS + Fargate project";
+    }
 }
