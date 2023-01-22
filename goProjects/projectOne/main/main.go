4dd4343872eaac59477767153645caf3f4779b70
MayyarAA <mayyar1235@gmail.com>
2023-01-22 18:10:56 -0500

    example of if & for in go

--- goProjects/projectOne/main/main.go
+++ goProjects/projectOne/main/main.go
@@ -1,8 +1,34 @@
 package main
 
 import "fmt"
+import "rsc.io/quote"
 
 func main(){
 	fmt.Println(" ss")	
-	
+	fmt.Println(quote.Go())
+
+	// declaring vars
+	var v1,v2 int = 10, 22;
+	v3 := "v3 variable shorthand declared"
+	fmt.Println(v1);
+	fmt.Println(v2);
+	fmt.Println(v3);	
+	// secondHelperExampleForLoop()
+	methodWIfExample(10)
+}
+
+// if example
+func methodWIfExample(data int){
+	if data % 2 == 0 {
+		fmt.Println("The data given is event: ", data)
+	}
+}
+
+// for example
+func secondHelperExampleForLoop(){
+	// go's looping construct: only has forloops
+
+	for i:=0; i< 10; i++{
+		fmt.Println("via loop prinring: ", i)
+	}
 }
